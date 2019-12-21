04e62a7353e1c2e603ecf214fe405189df5193b6
Barak <barak102@gmail.com>
2019-12-21 12:56:40 +0200

    Updates

---
+++ precache-manifest.87f558da0937f18f464009b46aa4d777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed07e82e7882b785c6524de9da128c85",
+    "url": "/vachile-app/index.html"
+  },
+  {
+    "revision": "b8ce7574dc5b46e7fa7b",
+    "url": "/vachile-app/static/css/main.56433497.chunk.css"
+  },
+  {
+    "revision": "066f8ff8997de62133e7",
+    "url": "/vachile-app/static/js/2.ff94f62e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vachile-app/static/js/2.ff94f62e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8ce7574dc5b46e7fa7b",
+    "url": "/vachile-app/static/js/main.6ce4d7a6.chunk.js"
+  },
+  {
+    "revision": "c90443e3e0d81971fbad",
+    "url": "/vachile-app/static/js/runtime-main.78ea33bd.js"
+  }
+]);
